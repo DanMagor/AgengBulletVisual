8071705bf699f69c1e286c17a456a3e898bf84be
DanMagor <anybismaat@gmail.com>
2020-06-02 16:32:31 +0300

    second scene added

---
+++ Assets/TestParkour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParkour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] private float speed = 1f;
+    private bool isTrick = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTrick) return;
+        transform.position += transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Obstacle"))
+        {
+            return;
+        }
+        isTrick = true;
+        GetComponent<Animator>().SetTrigger("Dash");
+    }
+}
